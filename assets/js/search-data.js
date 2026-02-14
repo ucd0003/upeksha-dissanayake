3bcd311b53a5dffd4e366ae3753e464c8a03885f
ucd0003 <84815294+ucd0003@users.noreply.github.com>
2026-02-14 17:53:44 +0000

    Deploying to gh-pages from @ ucd0003/upeksha-dissanayake@26424a5e5695db248e77246636baed91a8efeacb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -421,9 +421,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/upeksha-dissanayake/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-effect-of-cancer-mutations-on-dna-glycosylase-mutyh",
+          title: 'Effect of Cancer Mutations on DNA Glycosylase (MUTYH)',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/upeksha-dissanayake/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
