77c86b943f5ef120db81b75aa58ea45594c3d3ed
ucd0003 <84815294+ucd0003@users.noreply.github.com>
2026-02-19 04:49:03 +0000

    Deploying to gh-pages from @ ucd0003/upeksha-dissanayake@f0f2e83976140555cd5b3648b7c76a2d18045f95 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/upeksha-dissanayake/publications/";
           },
+        },{id: "nav-projects",
+          title: "Projects",
+          description: "A collection of computational chemistry, molecular modeling, and AI-driven research projects, highlighting my work across enzyme mechanisms, molecular simulations, and data-driven drug discovery.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/upeksha-dissanayake/projects/";
+          },
         },{id: "nav-resume",
           title: "Resume",
           description: "",
@@ -407,46 +414,21 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/upeksha-dissanayake/projects/1_project/";
-            },},{id: "projects-effect-of-cancer-mutations-on-dna-glycosylase-mutyh",
-          title: 'Effect of Cancer Mutations on DNA Glycosylase (MUTYH)',
-          description: "",
+            },},{id: "projects-effects-of-cancer-mutations-on-dna-glycosylase-mutyh",
+          title: 'Effects of Cancer Mutations on DNA Glycosylase (MUTYH)',
+          description: "This project investigates how cancer-associated mutations alter the structural stability, dynamics, and allosteric communication networks of the MUTYH DNA repair enzyme.",
           section: "Projects",handler: () => {
               window.location.href = "/upeksha-dissanayake/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/upeksha-dissanayake/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/upeksha-dissanayake/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-understanding-mechanisms-of-non-contacting-non-interfacial-mutations-in-protein-protein-compensations",
+          title: 'Understanding mechanisms of non-contacting/non-interfacial mutations in protein-protein compensations',
+          description: "This project investigates how distant, non-contacting mutations modulate protein–protein interactions through long-range allosteric communication and compensatory network effects.",
           section: "Projects",handler: () => {
               window.location.href = "/upeksha-dissanayake/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/upeksha-dissanayake/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
+            },},{id: "projects-computational-characterization-of-the-dna-repair-mechanism",
+          title: 'Computational Characterization of the DNA Repair Mechanism',
+          description: "This project is about studying DNA Repair mechanism of Primase-Polymerase C and initiation of DNA primer synthesis by CRISPR Associated Primase Polymerase (CAPP) enzymes",
           section: "Projects",handler: () => {
               window.location.href = "/upeksha-dissanayake/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/upeksha-dissanayake/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/upeksha-dissanayake/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
